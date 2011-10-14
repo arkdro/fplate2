bef14f010242b81e7c4675eaeec0aa9c48f7c97a
unknown <user1@localhost>
2011-10-14 23:34:18 +0400

    functor Fill added in plate.ml

--- src/plate.ml
+++ src/plate.ml
@@ -2,8 +2,63 @@ module type Item = sig
   type t
   val create : t -> t
   val to_string : t -> string
+  val cmp : t -> t -> bool
 end
+(* ---------------------------------------------------------------------- *)
+module Fill (Item : Item) = struct
+  type 'a row = Norow | Row of 'a
+  type 'a cell = Nocell | Cell of 'a
+  let prev_row plate y =
+    if y = 0 then
+      Norow
+    else
+      Row plate.(y-1)
+  let next_row plate y =
+    if y >= Array.length plate then
+      Norow
+    else
+      Row plate.(y+1)
+  let prev_cell row x =
+    if x = 0 then
+      Nocell
+    else
+      Cell row.(x-1)
+  let next_cell row x =
+    if x >= Array.length row then
+      Nocell
+    else
+      Cell row.(x+1)
+  let rec find_leftmost row i data =
+    (* TODO: return prev i, not cur i *)
+    match prev_cell row i with
+      | Nocell ->
+        i 
+      | Cell x when Item.cmp x data ->
+        find_leftmost row (i-1) data
+      | Cell _ ->
+        i
+  let rec find_rightmost row i data =
+    (* TODO: return prev i, not cur i *)
+    match next_cell row i with
+      | Nocell ->
+        i
+      | Cell x when Item.cmp x data ->
+        find_rightmost row (i+1) data
+      | Cell _ ->
+        i
+  let update_cells row left right = row
+  let update_cur_row row i cur_data new_data =
+    let left = find_leftmost row i cur_data in
+    let right = find_rightmost row i cur_data in
+    update_cells row left right
 
+    (* plate: plate, n: new item, (x;y): coordinates *)
+  let fill_step plate x y new_data =
+    let cur_data = plate.(y).(x) in
+    let new_cur_row = update_cur_row plate.(y) x cur_data new_data
+    in new_cur_row
+end
+(* ---------------------------------------------------------------------- *)
 module Plate (Item : Item) : sig
   type a
   val gen : Item.t -> int -> int -> a
@@ -11,6 +66,7 @@ module Plate (Item : Item) : sig
 end = struct
   type a = Item.t array array
   type t = Item.t
+  (* let cur_point_size x = ??? static var *)
   let gen_line f psz len =
     Array.init len (fun _ -> f psz)
   let rec gen_matrix2 acc f psz w = function
@@ -23,7 +79,7 @@ end = struct
   let gen psz w h = gen_matrix Item.create psz w h
   let to_string_row row = Array.map Item.to_string row
   let to_string p = Array.map to_string_row p
-  let rehash plate x y n =
-    (* plate - plate, n - new value, (x;y) - coordinates *)
-    ()
+  module F1 = Fill(Item)
+  let fill_step plate x y n = F1.fill_step plate x y n
+
 end
