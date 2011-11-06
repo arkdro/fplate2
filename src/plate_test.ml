adabb90f26fa071a15614123d96d241e2927e0b2
unknown <user1@localhost>
2011-11-06 23:18:21 +0400

    aux sub moved into real sub; check for restult stat and exit on finished result

--- src/plate_test.ml
+++ src/plate_test.ml
@@ -10,21 +10,27 @@ end = struct
   module Loop_test : sig
     val loop : P1.a -> int -> int -> int -> unit
   end = struct
-    let rec loop2 data psz cnt x =
-      let point = Point.create psz in
-      Printf.printf "loop2 data: cnt=%d\n" cnt;
-      Printf.printf "point=%s\n" (Point.to_string point);
-      match cnt with
-        | 0 -> ()
-        | _ ->
-          let _ = P1.fill_step data x 0 point in
-          Printf.printf "cur plate after fill_step:\n%s\n" (P1.to_string data);
-          let stat_cnt = P1.fill_step_count data x 0 in
-          Printf.printf "cur plate stat: %d\n" stat_cnt;
-          Printf.printf "cur plate after fill_step_count:\n%s\n" (P1.to_string data);
-          loop2 data psz (cnt-1) x
-
     let loop data psz w h =
+      let rec loop2 data psz cnt x =
+        let point = Point.create psz in
+        Printf.printf "loop2 data: cnt=%d\n" cnt;
+        Printf.printf "point=%s\n" (Point.to_string point);
+        match cnt with
+          | 0 -> ()
+          | _ ->
+            let _ = P1.fill_step data x 0 point in
+            Printf.printf "cur plate after fill_step:\n%s\n" (P1.to_string data);
+            let stat_cnt = P1.fill_step_count data x 0 in
+            Printf.printf "cur plate stat: %d\n" stat_cnt;
+            Printf.printf "cur plate after fill_step_count:\n%s\n"
+              (P1.to_string data);
+            (
+              if stat_cnt = w * h then
+                ()
+              else
+                loop2 data psz (cnt-1) x
+            )
+      in
       Printf.printf "loop init:\npoint size = %d\n" psz;
       Printf.printf "%s\n" (P1.to_string data);
       let start_x = Random.int w in
