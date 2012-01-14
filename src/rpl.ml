6f4d4119271915733026f3d131c6159c6e55f1e3
unknown <user1@localhost>
2012-01-14 20:19:14 +0400

    add ccl_test
    ccl_test flag added

--- src/rpl.ml
+++ src/rpl.ml
@@ -4,10 +4,11 @@ open Rinit
 open Point
 (* open Point2 *)
 open Plate
+open Ccl_test
 module P1 = Plate (Point)
 (* module P2 = Plate (Point2) *)
 
-let get_pars ifile ofile wr hr pszr tr sr =
+let get_pars ifile ofile wr hr pszr tr sr ct =
   let usage_text = "possible parameters:" in
   let pars = [
     ("-i", Set_string ifile, "input file");
@@ -16,6 +17,7 @@ let get_pars ifile ofile wr hr pszr tr sr =
     ("-h", Set_int hr, "height");
     ("-psz", Set_int pszr, "point size");
     ("-sr", Set_int sr, "spread ratio (0..100)");
+    ("-ct", Set_int ct, "ccl test level (0..2)");
     ("-t", Set tr, "test loop with given w, h, psz")
   ] in
   let _ = parse pars (fun x -> ()) usage_text in
@@ -40,12 +42,15 @@ let main () =
   let wr = ref 3 in
   let hr = ref 3 in
   let test = ref false in
-  get_pars ifile ofile wr hr pszr test s_ratio;
+  let ccl_test = ref 0 in
+  get_pars ifile ofile wr hr pszr test s_ratio ccl_test;
   let _ = Rinit.r_init in
-  Printf.printf "w=%d, h=%d, psz=%d, test=%b, sratio=%d\n"
-    !wr !hr !pszr !test !s_ratio;
+  Printf.printf "w=%d, h=%d, psz=%d, test=%b, sratio=%d, ccl test=%d\n"
+    !wr !hr !pszr !test !s_ratio !ccl_test;
   if !test = true then
     Plate_test.main !wr !hr !pszr !s_ratio
+  else if !ccl_test > 0 then
+    Ccl_test.main !wr !hr !pszr !s_ratio !ccl_test
   else
     (* let res = P2.gen point_size width height in *)
     let res = P1.gen !pszr !wr !hr !s_ratio in
