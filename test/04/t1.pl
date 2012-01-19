a5ef07e419d72efcad6015b3f63e04863c7739fe
unknown <user1@localhost>
2012-01-19 23:34:15 +0400

    compare result by own function

--- test/04/t1.pl
+++ test/04/t1.pl
@@ -4,7 +4,8 @@
 # ccl8 for every possible point. Compares input ccl result and PDL ccl8.
 #
 use strict;
-use 5.010;
+use Data::Compare;
+use Data::Dumper;
 use PDL;
 use PDL::Image2D;
 
@@ -43,17 +44,11 @@ for my $cell (sort {$a<=>$b} keys %$src_res){
 	my $cur_src = make_src_data($src_data, $cell);
 	my $cur_src_p = pdl(@$cur_src);
 	my $res_cc8 = cc8compt($cur_src_p);
-	my $res_cc8_norm = normalize_ccl8_res($res_cc8);
-	my $cur_src_res = normalize_src_res($src_res->{$cell});
-	my $src_res_p = pdl($cur_src_res);
-	my @cur_src_res_swapped = @$cur_src_res;
-	swap_index($res_cc8_norm, \@cur_src_res_swapped);
-	if(@$res_cc8_norm ~~ @cur_src_res_swapped){
+	if(compare($res_cc8, $src_res->{$cell})){
 		print "matched data\n";
 	}else{
 		print "not matched data\n";
 		print "input data:\n"   . $cur_src_p . "\n";
-		print "input result:\n" . $src_res_p . "\n";
 		print "cc8 result:\n"   . $res_cc8   . "\n";
 	}
 }
@@ -70,60 +65,38 @@ for my $row (@$src_data){
 return \@res;
 }
 
-sub normalize_src_res {
-my($dat) = @_;
-my %keys;
-for my $row (@$dat){
-	for my $cell (@$row){
-		$keys{$cell} = 1;
-	}
+sub compare {
+my($cc8, $src) = @_;
+my @cc8_list = $cc8->list;
+my $src_p = pdl(@$src);
+my @src_list = $src_p->list;
+print "cc8:\n" . $cc8 . "\n";
+print "src:\n" . $src_p . "\n";
+return if @cc8_list != @src_list; # compare length only
+my(@checked_c, @checked_s);
+for(my $i = 0; $i < @cc8_list && $i < @src_list; $i++){
+	print "i=$i\n";
+	my $label_c = $cc8_list[$i];
+	my $label_s = $src_list[$i];
+	print "label c: $label_c\n";
+	print "label s: $label_s\n";
+	next if $checked_c[$label_c] && $checked_s[$label_s];
+	my $cell_c = fetch_cells(\@cc8_list, $label_c);
+	my $cell_s = fetch_cells(\@src_list, $label_s);
+	print "cell_c:\n" . Dumper($cell_c) . "\n";
+	print "cell_s:\n" . Dumper($cell_s) . "\n";
+	return unless Data::Compare::Compare($cell_s, $cell_c);
+	$checked_c[$label_c] = $cell_c;
+	$checked_s[$label_s] = $cell_s;
 }
-my @k = keys %keys;
-my $out_keys = normalize_items(\@k);
-my @res;
-for my $row (@$dat){
-	my @r2 = @$row;
-	map { $_ = $out_keys->{$_} } @r2;
-	push @res, @r2;
-}
-return \@res;
-}
-
-sub normalize_items {
-my ($in_dat) = @_;
-my @sk = sort {$a <=> $b} @$in_dat;
-my %out_keys;
-for(my $i = 0; $i < @sk; $i++){
-	$out_keys{$sk[$i]} = $i;
-}
-return \%out_keys;
+return 1;
 }
 
-sub normalize_ccl8_res {
-my($res) = @_;
-my @lst = $res->list;
-my %k;
-map {$k{$_} = 1} @lst;
-my @k2 = keys %k;
-my $out_keys = normalize_items(\@k2);
-map { $_ = $out_keys->{$_} } @lst;
-return \@lst;
+sub fetch_cells {
+my($list, $label) = @_;
+my @dat;
+for(my $i=0; $i < @$list; $i++){
+	push @dat, $i if $list->[$i] == $label;
 }
-
-sub swap_index {
-my ($res_cc8_norm, $cur_src_res) = @_;
-for(my $i = 0; $i < @$res_cc8_norm; $i++){
-	my $ic = $res_cc8_norm->[$i];
-	my $is = $cur_src_res->[$i];
-	if($ic != $is){
-		swap_one_index($cur_src_res, $ic, $is);
-		#swap_index($res_cc8_norm, $cur_src_res);
-		last;
-	}
-}
-}
-
-sub swap_one_index {
-my($cur_src_res, $ic, $is) = @_;
-
+return \@dat;
 }
