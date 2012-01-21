5ad967287582d063d8a63d95031a0d72a0c42aae
unknown <user1@localhost>
2012-01-21 22:11:17 +0400

    removed unnecessary types and values from a signature of a functor's parameter; a few comments added; a few typos corrected.

--- src/ccl/ccl.ml
+++ src/ccl/ccl.ml
@@ -1,35 +1,13 @@
 (* connected component labeling
  * based on (iciap99lu.pdf)
- * "A simple and efficient connected component labeling algorithm"
+ * "A simple and efficient connected components labeling algorithm"
  * by Luidgi Di Stefano, Andrea Bulgarelli
  *)
 (* ---------------------------------------------------------------------- *)
 module type ItemSig = sig
   type t
-  val create      : int -> t
-  val create_uniq : t
   val to_string   : t -> string
-  val to_string2   : t -> string
   val cmp         : t -> t -> bool
-  val cmp_iter    : t -> t -> bool
-  val add_push    : t -> t
-  val set_iter    : t -> int -> t
-  val copy_iter   : t -> t -> t
-  val add_iter    : t -> t
-  val clean       : t -> t
-
-  type c_cnt
-  type c_key = t
-  val c_empty     : c_cnt
-  val c_set       : c_key -> int -> c_cnt -> c_cnt
-  val c_inc_n     : c_key -> int -> c_cnt -> c_cnt
-  val c_inc       : c_key -> c_cnt -> c_cnt
-  val c_inc_iter  : int -> c_key -> c_cnt -> c_cnt
-  val c_inc_iter_target : c_key -> c_key -> c_cnt -> c_cnt
-  val c_to_string : c_cnt -> string
-  val separate_item     : c_key -> c_cnt -> (int * c_cnt)
-  val get_max           : c_cnt -> (c_key * int)
-  val keys              : c_cnt -> c_key list
 end
 (* ---------------------------------------------------------------------- *)
 (* base module has 6-connectivity *)
@@ -223,13 +201,13 @@ module Ccl (Item : ItemSig) = struct
   let up_cells conn_ways plate x y =
     match prev_row plate y with
       | Norow -> []
-      | Row row when conn_ways = 4 ->
+      | Row row when conn_ways = 4 ->   (* 4-connectivity *)
         [(cur_cell row x, x, y-1)]
-      | Row row when conn_ways = 8 ->
+      | Row row when conn_ways = 8 ->   (* 8-connectivity *)
         [prev_cell row x, x-1, y-1;
          cur_cell row x, x, y-1;
          next_cell row x, x+1, y-1]
-      | Row row ->
+      | Row row ->                      (* 6-connectivity, default *)
         if even y
         then
           (* x-1; x *)
@@ -464,7 +442,7 @@ module Ccl (Item : ItemSig) = struct
 
   (* - - - labeling - - - - - - - - - - - - - - - - - - - - - -*)
 
-  (* do a connected component labeling *)
+  (* do a connected components labeling *)
   let ccl avail_cells conn_ways w h list =
     let plate = init_ccl_matrix w h list in
     let f cell = labeling cell conn_ways plate w h in
