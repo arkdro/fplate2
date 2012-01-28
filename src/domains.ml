85dee263da40b1dcdb22d57f7b43f0a28943deb1
unknown <user1@localhost>
2012-01-28 13:12:11 +0400

    added domains.ml file

---
+++ src/domains.ml
@@ -0,0 +1,11 @@
+(*
+  build a simple graph-like structure based on the ccl result.
+  The structure contains a list (or hash?) of nodes. Each node is:
+  label, number of cells, neighbour nodes.
+
+  Also, operations: merge two nodes.
+*)
+
+module Domains = struct
+
+end
