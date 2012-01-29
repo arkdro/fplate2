52496e1fdaf76a869c5d971239dc740da4aa2fab
unknown <user1@localhost>
2012-01-29 23:01:42 +0400

    added command line parameters: verbose, loops; use verbose and loops in ccl_test module.

--- src/ccl_test.ml
+++ src/ccl_test.ml
@@ -7,7 +7,7 @@ module D1 = Domains (Point)
 module P1 = Plate (Point)
 module C1 = Ccl (Point)
 module Ccl_test : sig
-  val main : int -> int -> int -> int -> int -> unit
+  val main : int -> int -> int -> int -> int -> int -> int -> unit
 end = struct
 (* ---------------------------------------------------------------------- *)
   module Loop_test : sig
@@ -26,29 +26,42 @@ end = struct
       in
       points, res_ccl_list
   end
-(* ---------------------------------------------------------------------- *)
-  let main width height point_size spread_ratio ccl_type =
-    (* let _ = Rinit.r_init in *)
-    Printf.printf "main: %d, %d, %d, %d\n" width height point_size ccl_type;
+  (* ---------------------------------------------------------------------- *)
+  let one_step width height point_size spread_ratio ccl_type verbose =
+    if verbose > 0 then (
+      Printf.printf "one_step: %d, %d, %d, %d\n"
+        width height point_size ccl_type);
     let data = P1.gen point_size width height spread_ratio in
-    Printf.printf "begin data:\n%s\n" (P1.to_string data);
+    if verbose > 0
+    then Printf.printf "begin data:\n%s\n" (P1.to_string data);
     let data_list = P1.get_data_list data in
     let points, res = Loop_test.loop data_list point_size width
       height ccl_type in
     IFDEF DOMAIN_DEBUG THEN (
       let domains = D1.create_domains points res in
-      Printf.printf "main, fused domains:\n";
+      Printf.printf "one_step, fused domains:\n";
       D1.dump_plate domains
     ) ENDIF;
-    Printf.printf "main, ccl result:\n";
-    let list2 = List.combine points res in
-    let f (point, ccl_item) =
-      Printf.printf "main, ccl result, cell: %s\n" (Point.to_string point);
-      Printf.printf "main, ccl result, ccl_item:\n";
-      C1.dump_one_ccl width height ccl_item;
-      Printf.printf "main, ccl result, done\n"
-    in
-    List.iter f list2;
+    if verbose > 0 then (
+      Printf.printf "one_step, ccl result:\n";
+      let list2 = List.combine points res in
+      let f (point, ccl_item) =
+        Printf.printf "one_step, ccl result, cell: %s\n"
+          (Point.to_string point);
+        Printf.printf "one_step, ccl result, ccl_item:\n";
+        C1.dump_one_ccl width height ccl_item;
+        Printf.printf "one_step, ccl result, done\n"
+      in
+      List.iter f list2;
+      Printf.printf "one_step end\n"
+    )
+  (* ---------------------------------------------------------------------- *)
+  let main width height point_size spread_ratio ccl_type verbose loops =
+    Printf.printf "main: %d, %d, %d, %d\n" width height point_size ccl_type;
+    for i = 0 to loops-1 do
+      Printf.printf "main: i=%d\n" i;
+      one_step width height point_size spread_ratio ccl_type verbose
+    done;
     Printf.printf "main end\n"
-end
 (* ---------------------------------------------------------------------- *)
+end
--- src/rpl.ml
+++ src/rpl.ml
@@ -8,7 +8,7 @@ open Ccl_test
 module P1 = Plate (Point)
 (* module P2 = Plate (Point2) *)
 
-let get_pars ifile ofile wr hr pszr tr sr ct =
+let get_pars ifile ofile wr hr pszr tr sr ct ctype loops verbose =
   let usage_text = "possible parameters:" in
   let pars = [
     ("-i", Set_string ifile, "input file");
@@ -17,8 +17,11 @@ let get_pars ifile ofile wr hr pszr tr sr ct =
     ("-h", Set_int hr, "height");
     ("-psz", Set_int pszr, "point size");
     ("-sr", Set_int sr, "spread ratio (0..100)");
-    ("-ct", Set_int ct, "ccl type - 4, 6 (default), 8");
-    ("-t", Set tr, "test loop with given w, h, psz")
+    ("-ct", Unit (fun x -> ct := true), "ccl test");
+    ("-ctype", Set_int ctype, "ccl type - 4, 6 (default), 8");
+    ("-loops", Set_int loops, "loops (default: 1)");
+    ("-t", Set tr, "test loop with given w, h, psz");
+    ("-v", Set_int verbose, "verbose (default: 1)")
   ] in
   let _ = parse pars (fun x -> ()) usage_text in
   Printf.printf "output file: %s\n" !ofile;
@@ -37,20 +40,24 @@ let get_pars ifile ofile wr hr pszr tr sr ct =
 let main () =
   let ifile = ref "" in
   let ofile = ref "" in
+  let loops = ref 1 in
   let s_ratio = ref 0 in
   let pszr = ref 8 in
   let wr = ref 3 in
   let hr = ref 3 in
   let test = ref false in
   let ccl_type = ref 0 in
-  get_pars ifile ofile wr hr pszr test s_ratio ccl_type;
+  let ccl_test = ref false in
+  let verbose = ref 1 in
+  get_pars ifile ofile wr hr pszr test s_ratio ccl_test ccl_type loops
+    verbose;
   let _ = Rinit.r_init in
-  Printf.printf "w=%d, h=%d, psz=%d, test=%b, sratio=%d, ccl type=%d\n"
-    !wr !hr !pszr !test !s_ratio !ccl_type;
+  Printf.printf "w=%d, h=%d, psz=%d, test=%b, sratio=%d, ccl type=%d, ccl test=%B, loops=%d, verbose=%d\n"
+    !wr !hr !pszr !test !s_ratio !ccl_type !ccl_test !loops !verbose;
   if !test = true then
     Plate_test.main !wr !hr !pszr !s_ratio
-  else if !ccl_type > 0 then
-    Ccl_test.main !wr !hr !pszr !s_ratio !ccl_type
+  else if !ccl_test = true then
+    Ccl_test.main !wr !hr !pszr !s_ratio !ccl_type !verbose !loops
   else
     (* let res = P2.gen point_size width height in *)
     let res = P1.gen !pszr !wr !hr !s_ratio in
