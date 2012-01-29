d8666bda3458f23c8c5b6d8bd5654fadbb4b618b
unknown <user1@localhost>
2012-01-29 18:39:31 +0400

    added dump_plate inside IFDEF DOMAIN_DEBUG to be used for plate fuse debug

--- src/domains.ml
+++ src/domains.ml
@@ -10,6 +10,9 @@
 module type ItemSig = sig
   type t
   val cmp         : t -> t -> bool
+    IFDEF DOMAIN_DEBUG THEN
+  val to_string   : t -> string
+    ENDIF
 end
 (* ---------------------------------------------------------------------- *)
 module Domains (Item : ItemSig) = struct
@@ -17,6 +20,25 @@ module Domains (Item : ItemSig) = struct
   type label2 = Item.t * int
   type node = {id: label; item: Item.t; sum: int; adj: (label, bool) Hashtbl.t}
   type doms = (label, node) Hashtbl.t
+
+      IFDEF DOMAIN_DEBUG THEN
+  let to_string plate =
+    let row_to_string row =
+      let f = function
+        | None -> "_"
+        | Some (cell, label) ->
+          Printf.sprintf "%s,%d" (Item.to_string cell) label
+      in
+      let lst = Array.to_list row in
+      let lstr = List.map f lst in
+      String.concat "; " lstr
+    in
+    let lst = Array.to_list plate in
+    let str_list = List.map row_to_string lst in
+    String.concat "\n" str_list
+
+  let dump_plate plate = Printf.printf "plate:\n%s\n" (to_string plate)
+    ENDIF
 
   (* get list of plates, use first plate to get width and height of
      all the plates in the list *)
