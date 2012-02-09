9c37319493ce1168b17646da728d2b464e66a956
unknown <user1@localhost>
2012-02-09 23:50:07 +0400

    added dump_ct_ccl

--- src/ccl/ct_ccl.ml
+++ src/ccl/ct_ccl.ml
@@ -21,6 +21,31 @@ end
 module Ct_ccl (Item : ItemSig) = struct
   exception Array_size_mismatch
 
+  let ct_ccl_to_string a =
+    let w = Array2.dim1 a in
+    let h = Array2.dim2 a in
+    let row_to_str y =
+      let rec aux x acc =
+        if x >= 0
+        then
+          let item_str = Printf.sprintf "%d" a.{x, y} in
+          aux (x-1) (item_str :: acc)
+        else acc
+      in
+      let lst = aux (w-1) [] in
+      String.concat " " lst
+    in
+    let rec aux_rows y racc =
+      if y >= 0
+      then aux_rows (y-1) ((row_to_str y) :: racc)
+      else racc
+    in
+    let rlst = aux_rows (h-1) [] in
+    String.concat "\n" rlst
+
+  let dump_ct_ccl a =
+    Printf.printf "dump_ct_ccl:\n%s\n" (ct_ccl_to_string a)
+
   (* fill a 2d matrix with data from a flat list *)
   let init_ccl_matrix w h list =
 
@@ -51,6 +76,9 @@ module Ct_ccl (Item : ItemSig) = struct
   (* do a connected components labeling *)
   let ccl avail_cells conn_ways w h list =
     let plate = init_ccl_matrix w h list in
+    IFDEF CTCCL_DUMP_DEBUG THEN (
+      dump_ct_ccl plate
+    ) ENDIF;
     let f cell = labeling cell conn_ways plate w h in
     List.map f avail_cells
    
