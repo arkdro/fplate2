cc4ff914b4d583a6b0dd2f3b7d9d4bfa40dc192b
unknown <user1@localhost>
2012-02-09 23:50:57 +0400

    added parameter for Ct_ccl_test

--- src/rpl.ml
+++ src/rpl.ml
@@ -5,10 +5,11 @@ open Point
 (* open Point2 *)
 open Plate
 open Ccl_test
+open Ct_ccl_test
 module P1 = Plate (Point)
 (* module P2 = Plate (Point2) *)
 
-let get_pars ifile ofile wr hr pszr tr sr ct ctype loops verbose =
+let get_pars ifile ofile wr hr pszr tr sr ct ctt ctype loops verbose =
   let usage_text = "possible parameters:" in
   let pars = [
     ("-i", Set_string ifile, "input file");
@@ -18,6 +19,7 @@ let get_pars ifile ofile wr hr pszr tr sr ct ctype loops verbose =
     ("-psz", Set_int pszr, "point size");
     ("-sr", Set_int sr, "spread ratio (0..100)");
     ("-ct", Unit (fun x -> ct := true), "ccl test");
+    ("-ctt", Unit (fun x -> ctt := true), "ct ccl test");
     ("-ctype", Set_int ctype, "ccl type - 4, 6 (default), 8");
     ("-loops", Set_int loops, "loops (default: 1)");
     ("-t", Set tr, "test loop with given w, h, psz");
@@ -48,14 +50,18 @@ let main () =
   let test = ref false in
   let ccl_type = ref 0 in
   let ccl_test = ref false in
+  let ct_ccl_test = ref false in
   let verbose = ref 1 in
-  get_pars ifile ofile wr hr pszr test s_ratio ccl_test ccl_type loops
-    verbose;
+  get_pars ifile ofile wr hr pszr test s_ratio ccl_test ct_ccl_test
+    ccl_type loops verbose;
   let _ = Rinit.r_init in
-  Printf.printf "w=%d, h=%d, psz=%d, test=%b, sratio=%d, ccl type=%d, ccl test=%B, loops=%d, verbose=%d\n"
-    !wr !hr !pszr !test !s_ratio !ccl_type !ccl_test !loops !verbose;
+  Printf.printf "w=%d, h=%d, psz=%d, test=%b, sratio=%d, ccl type=%d, ccl test=%B, ct ccl test=%B, loops=%d, verbose=%d\n"
+    !wr !hr !pszr !test !s_ratio !ccl_type !ccl_test !ct_ccl_test
+    !loops !verbose;
   if !test = true then
     Plate_test.main !wr !hr !pszr !s_ratio
+  else if !ct_ccl_test = true then
+    Ct_ccl_test.main !wr !hr !pszr !s_ratio !ccl_type !verbose !loops
   else if !ccl_test = true then
     Ccl_test.main !wr !hr !pszr !s_ratio !ccl_type !verbose !loops
   else
