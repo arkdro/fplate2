770e4b6dad638e617a91d49201e9be2384eb4a64
unknown <user1@localhost>
2012-02-12 23:03:05 +0400

    adding labeling ct_ccl

--- src/ccl/ct_ccl.ml
+++ src/ccl/ct_ccl.ml
@@ -7,11 +7,15 @@
 open Bigarray
 open Array2
 
+open Ct_next_item.Ct_next_item
+
 (* ---------------------------------------------------------------------- *)
 module type ItemSig = sig
   type t
   val cmp         : t -> t -> bool
   val value       : t -> int
+  val filler      : int
+  val empty       : int
     IFDEF CTCCL_DUMP_DEBUG THEN
   val to_string   : t -> string
     ENDIF
@@ -46,7 +50,8 @@ module Ct_ccl (Item : ItemSig) = struct
   let dump_ct_ccl a =
     Printf.printf "dump_ct_ccl:\n%s\n" (ct_ccl_to_string a)
 
-  (* fill a 2d matrix with data from a flat list *)
+  (* fill a 2d matrix with data from a flat list. Return matrix and
+     extra border vectors *)
   let init_ccl_matrix w h list =
 
     (* transforms linear index to x,y coordinates *)
@@ -56,6 +61,12 @@ module Ct_ccl (Item : ItemSig) = struct
       let x = i mod w in
       (x,y)
     in
+
+    (* extra borders for marking surrounding background pixels *)
+    let up = Array1.fill (Array1.create int c_layout (w+2)) Item.filler in
+    let down = Array1.fill (Array1.create int c_layout (w+2)) Item.filler in
+    let left = Array1.fill (Array1.create int c_layout (h+2)) Item.filler in
+    let right = Array1.fill (Array1.create int c_layout (h+2)) Item.filler in
 
     let a1 = Array2.create int c_layout w h in
     let rec aux0 idx = function
@@ -68,18 +79,36 @@ module Ct_ccl (Item : ItemSig) = struct
       | [] -> ()
     in
     aux0 0 list;
-    a1
+    (a1, up, right, down, left)
 
-
-  let labeling cell conn_ways plate w h = ()
+  (* - - - labeling- - - - - - - - - - - - - - - - - - - - - - - - - *)
+  let labeling cell conn_ways data w h =
+    let (plate, b_up, b_right, b_down, b_left) = data in
+    let labels = Array2.fill (Array2.create int c_layout w h) Item.empty in
+    let is_bg (x, y) = false in
+    let has_label (x, y) = false in
+    let has_mark (x, y) = false in
+    let step_1 = () in
+    let step_2 = () in
+    let step_3 = () in
+    let aux x y =
+      if (not (has_label (x, y))) && (is_bg (up_coord w h x y))
+      then step_1
+      else if is_bg (down_coord w h x y) && (not (has_mark (down_coord w h x y)))
+      then step_2
+      else step_3
+    in
+    aux 0 0
+  (* - - - labeling- - - - - - - - - - - - - - - - - - - - - - - - - *)
 
   (* do a connected components labeling *)
   let ccl avail_cells conn_ways w h list =
-    let plate = init_ccl_matrix w h list in
+    let data = init_ccl_matrix w h list in
     IFDEF CTCCL_DUMP_DEBUG THEN (
+      let (plate, _b_up, _b_right, _b_down, _b_left) = data in
       dump_ct_ccl plate
     ) ENDIF;
-    let f cell = labeling cell conn_ways plate w h in
+    let f cell = labeling cell conn_ways data w h in
     List.map f avail_cells
    
   let ccl4 avail_cells w h list =
---
+++ src/ccl/ct_next_item.ml
@@ -0,0 +1,4 @@
+module Ct_next_item = struct
+  let up_coord w h x y = x, y
+  let down_coord w h x y = x, y
+end
