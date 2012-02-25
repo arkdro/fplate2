d324fedf61bd448f9a5f8c6dd2a809b75c009b1f
unknown <user1@localhost>
2012-02-25 23:15:59 +0400

    added prev_in_line_coor

--- src/ccl/ct_next_item.ml
+++ src/ccl/ct_next_item.ml
@@ -16,4 +16,9 @@ module Ct_next_item = struct
     then Some (0, y+1)
     else None
 
+  let prev_in_line_coor x y =
+    if x > 0
+    then x-1, y
+    else assert false                   (* should not happen *)
+
 end
